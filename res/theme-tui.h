0bcc14671aa2d84ec20f260582aca36bb901ccf1
Andrea Adami <andrea.adami@gmail.com>
2013-02-18 23:12:42 +0100

    theme-tui.h: add headers and include config.h
    for conformnance with other header files
    
    Signed-off-by: Andrea Adami <andrea.adami@gmail.com>

--- res/theme-tui.h
+++ res/theme-tui.h
@@ -1,3 +1,22 @@
+/*
+ *  kexecboot - A kexec based bootloader
+ *
+ *  Copyright (c) 2008-2011 Yuri Bushmelev <jay4mail@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
+#include "../config.h"
+
 #ifdef USE_TEXTUI
 /** TEXT UI colors **/
 #include "../termseq.h"
